edb219dfd30e6a21289a26aad88d757f5a0b1f62
Johan Bäckman <johan.c.backman@gmail.com>
2018-07-10 18:59:22 +0200

    DRY with some variables

--- LucyLossOfControl.lua
+++ LucyLossOfControl.lua
@@ -1,27 +1,38 @@
 -- https://www.mmo-champion.com/threads/1231654-Loss-of-Control-Interface-UI
 
 -- Move around the entire frame
-LossOfControlFrame:ClearAllPoints() LossOfControlFrame:SetPoint("CENTER",UIParent,"CENTER",75,100)
+LossOfControlFrame:ClearAllPoints()
+LossOfControlFrame:SetPoint("CENTER",UIParent,"CENTER",75,200)
+
+local effectText = select(5,LossOfControlFrame:GetRegions());
+local background = select(1,LossOfControlFrame:GetRegions());
+local icon = select(4,LossOfControlFrame:GetRegions());
+local topRedLine = select(2,LossOfControlFrame:GetRegions());
+local bottomRedLine = select(3,LossOfControlFrame:GetRegions());
+
 
 -- Move the icon around (if you want to center the frame based on the icon and not the countdown and red border)
-select(4,LossOfControlFrame:GetRegions()):ClearAllPoints ()
-select(4,LossOfControlFrame:GetRegions()):SetPoint("CENTER",UIParent,"CENTER",0,200)
+icon:ClearAllPoints();  --select(4,LossOfControlFrame:GetRegions()):ClearAllPoints()
+icon:SetPoint("CENTER",UIParent,"CENTER",0,200);  -- select(4,LossOfControlFrame:GetRegions()):SetPoint("CENTER",UIParent,"CENTER",0,200)
 
 
 -- Hide the background shadow
-select(1,LossOfControlFrame:GetRegions()):SetAlpha(0)
+background:SetAlpha(0)  --select(1,LossOfControlFrame:GetRegions()):SetAlpha(0)
 
 
 -- Hide the red frames
-select(2,LossOfControlFrame:GetRegions()):SetAlpha(0)
-select(3,LossOfControlFrame:GetRegions()):SetAlpha(0)
+-- select(2,LossOfControlFrame:GetRegions()):SetAlpha(0)
+-- select(3,LossOfControlFrame:GetRegions()):SetAlpha(0)
+topRedLine:SetAlpha(0);
+bottomRedLine:SetAlpha(0);
 
 
 -- Moves the effect text to centered under the icon
-select(5,LossOfControlFrame:GetRegions()):ClearAllPoints()
-select(5,LossOfControlFrame:GetRegions()):SetPoint("TOP",select(4,LossOfControlFrame:GetRegions()),"BOTTOM")
+-- select(5,LossOfControlFrame:GetRegions()):ClearAllPoints()
+-- select(5,LossOfControlFrame:GetRegions()):SetPoint("TOP",select(4,LossOfControlFrame:GetRegions()),"BOTTOM")
+effectText:ClearAllPoints();
+effectText:SetPoint("TOP", icon, "BOTTOM");
 
 -- Moves the duration text to be centered under the icon
-LossOfControlFrame.TimeLeft:SetAlpha(0)
-LossOfControlFrame.TimeLeft:SetPoint("TOP",select(4,LossOfControlFrame:GetRegions()),"BOTTOM")
-
+LossOfControlFrame.TimeLeft:SetAlpha(0);
+LossOfControlFrame.TimeLeft:SetPoint("TOP", effectText, "BOTTOM")
