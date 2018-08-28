fa9ea881c7acae0b82460b91252cd98b75b8b9ef
Johan Bäckman <johan.c.backman@gmail.com>
2018-08-28 10:47:50 +0200

    Remove a bunch of refactored code

--- LucyLossOfControl.lua
+++ LucyLossOfControl.lua
@@ -13,24 +13,20 @@ local timeLeft = LossOfControlFrame.TimeLeft;
 
 
 -- Move the icon around (if you want to center the frame based on the icon and not the countdown and red border)
-icon:ClearAllPoints();  --select(4,LossOfControlFrame:GetRegions()):ClearAllPoints()
-icon:SetPoint("CENTER",UIParent,"CENTER",0,200);  -- select(4,LossOfControlFrame:GetRegions()):SetPoint("CENTER",UIParent,"CENTER",0,200)
+icon:ClearAllPoints();
+icon:SetPoint("CENTER",UIParent,"CENTER",0,200);
 
 
 -- Hide the background shadow
-background:SetAlpha(0)  --select(1,LossOfControlFrame:GetRegions()):SetAlpha(0)
+background:SetAlpha(0);
 
 
 -- Hide the red frames
--- select(2,LossOfControlFrame:GetRegions()):SetAlpha(0)
--- select(3,LossOfControlFrame:GetRegions()):SetAlpha(0)
 topRedLine:SetAlpha(0);
 bottomRedLine:SetAlpha(0);
 
 
 -- Moves the effect text to centered under the icon
--- select(5,LossOfControlFrame:GetRegions()):ClearAllPoints()
--- select(5,LossOfControlFrame:GetRegions()):SetPoint("TOP",select(4,LossOfControlFrame:GetRegions()),"BOTTOM")
 effectText:ClearAllPoints();
 effectText:SetPoint("TOP", icon, "BOTTOM");
 effectText:SetScale(0.8);
